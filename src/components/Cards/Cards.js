acd7deb885dc4187c1382a9e835bf1b429da2801
Siddharth Teli <telisiddharth123@gmail.com>
2021-09-07 15:53:30 +0530

    Add: Style to id element of card Implemented:
    Because:

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,9 +3,24 @@ import './Cards.css'
 const Cards = ({id_card,name,price}) => {
     return(
     <div className="card-container">
-
+        <div className="header-card">
+           <h3 class="identifier">{id_card}</h3>
+           <h3>{name}</h3>
+        </div>
+        <div className="section-card">
+        <h3>Current rate:</h3>
+        <h3>{price}</h3>
+        </div>
+         
+         
     </div>)
 
+}
+
+Cards.defaultProps = {
+    id_card:1,
+    name:"BTC",
+    price:"12.30 Euro",
 }
 
 
