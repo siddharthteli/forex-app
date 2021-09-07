be26da505ca4c1c137057154e93c7fa21c321f00
Siddharth Teli <telisiddharth123@gmail.com>
2021-09-07 15:14:48 +0530

    Add:Cards component layout with props Implemented:
    Because:

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Cards from './components/Cards/Cards.js'
+function App() {
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,12 @@
+import './Cards.css'
+
+const Cards = ({id,name,price}) => {
+    return(
+    <div className="card-container">
+
+    </div>)
+
+}
+
+
+export default Cards;
