a91cf1ddc17d842ec6c0deeeb2574daa40d35008
Siddharth Teli <telisiddharth123@gmail.com>
2021-09-08 12:34:11 +0530

    Try:get api & mutiple card rending .  Describe the implementation Implemented:
    Because:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 import Container from './components/Container/Container.js'
 function App() {
+ 
   return (
     <div className="App">
       <Container />
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,15 +1,85 @@
 import Cards from  '../Cards/Cards.js'
+import axios from 'axios';
 import './Container.css'
-const Container =() => {
-    return(
-        <div className="grid-wrapper">
-           <Cards/>
-           <Cards/>
-           <Cards/>
-          
-        </div>
-    )
+import React, { useState } from 'react';
+import { Component } from 'react';
+
+// const Container = () => {
+//     const cartlist=[1,2,3,4,5];
+//     return(
+        
+//        cartlist.map((card) => 
+//            <h1>HEllo no is {card}</h1>
+//        )
+       
+       
+       
+//     )
+// }
+
+
+
+let cards_list=[];
+    let card;
+const Container =() =>{
+  const getapi= () => {
+        
+        axios.get("http://api.exchangeratesapi.io/v1/latest?access_key=ea27f11e67cc1a6cb6d066300def6963&format=1").then(
+            (response) => {
+                console.log("ENter response");
+                let rates=response.data.rates;
+                for (var key in rates) {
+                    card= {
+                        "name":key,
+                        "rate":rates[key],
+                    }
+                    cards_list.push(card);
+                    
+                }
+                
+                console.log(cards_list[0].name);
+                
+                
+            }
+
+           
+            
+        );
+        return (
+            cards_list.map((card) => 
+                       <h6>HEllo no is {card.name}</h6>
+                    )
+        )
+        
+    };
+  return  getapi();
+
 }
 
+
+
+// const Container =() => {
+//     const [count,setcount]=useState(1);
+//     React.useEffect(() => {
+//         axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ea27f11e67cc1a6cb6d066300def6963&format=1`).then((response) => {
+//           console.log("Get getting called")
+//           setcount(response);
+//         });
+//       }, []);
+//       if (!count) return null;
+//     return(
+        
+//         <div className="grid-wrapper">
+//             <h1>{count.rates} </h1>
+//            <Cards/>
+//            <Cards/>
+//            <Cards/>
+          
+//         </div>
+//     )
+// }
+
+
+ 
 
 export default Container;
